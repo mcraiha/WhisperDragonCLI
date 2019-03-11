9dbc4919df095121b030230be3190d8a8667d63c
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-03-11 22:14:43 +0200

    Add more options to CreateNewDialog
    Labels etc.

--- src/CreateNewDialog.cs
+++ src/CreateNewDialog.cs
@@ -5,6 +5,9 @@ namespace WhisperDragonCLI
 {
 	public static class CreateNewDialog
 	{
+		private static readonly int suggestedIterations = 100_000;
+		private static readonly string[] prfArray = new string[] {"SHA-512", "SHA-256"};
+
 		public static Dialog CreateNewFileDialog(Action okAction, Action cancelAction)
 		{
 			var ok = new Button(3, 14, LocMan.Get("Ok")) 
@@ -17,27 +20,50 @@ namespace WhisperDragonCLI
 				Clicked = cancelAction
 			};
 
-			var dialog = new Dialog (LocMan.Get("Login"), 60, 18, ok, cancel);
+			var dialog = new Dialog(LocMan.Get("Pbkdf2 settings"), 60, 20, ok, cancel);
 
-			var passwordFirstTime = new TextField ("") 
-			{
-				Secret = true,
-				X = 1, 
-				Y = 1,
-				Width = Dim.Fill (),
-				Height = 1
-			};
 
-			var passwordSecondTime = new TextField ("") 
+			var enterPasswordLabel = new Label(1, 1, LocMan.Get("Enter password:"));
+
+			var passwordFirstTime = new TextField("") 
 			{
 				Secret = true,
 				X = 1, 
 				Y = 2,
-				Width = Dim.Fill (),
+				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			dialog.Add(passwordFirstTime, passwordSecondTime);
+
+			var confirmPasswordLabel = new Label(1, 4, LocMan.Get("Confirm password:"));
+
+			var passwordSecondTime = new TextField("") 
+			{
+				Secret = true,
+				X = 1, 
+				Y = 5,
+				Width = Dim.Fill(),
+				Height = 1
+			};
+
+
+			var prfLabel = new Label(1, 7, LocMan.Get("Pseudo-Random Function:"));
+
+			var prfRadioSelection = new RadioGroup(1, 8, prfArray);
+
+			
+			var iterationsLabel = new Label(1, 11, LocMan.Get("How many iterations:"));
+
+			var iterationsAmount = new TextField (suggestedIterations.ToString()) 
+			{
+				X = 1, 
+				Y = 12,
+				Width = Dim.Fill(),
+				Height = 1
+			};
+
+
+			dialog.Add(enterPasswordLabel, passwordFirstTime, confirmPasswordLabel, passwordSecondTime, prfLabel, prfRadioSelection, iterationsLabel, iterationsAmount);
 
 			return dialog;
 		}
