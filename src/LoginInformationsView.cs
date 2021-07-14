af6d1c1aa0cc1523f2a2341672b47025997372eb
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-07-14 21:10:42 +0300

    Rename LoginInformationsWindow to LoginInformationsView
    Easier to understand

--- src/LoginInformationsView.cs
+++ src/LoginInformationsView.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 
 namespace WhisperDragonCLI
 {
-	public static class LoginInformationsWindow
+	public static class LoginInformationsView
 	{
-		public static void CreateLoginInformationsDialog(Window parentWindow)
+		public static View CreateView()
 		{
-			SectionBar.AddSectionBar(parentWindow, ContainerSection.LoginInformations);
-
 			List<string> stringList = new List<string>() { "some what long string", "and nice collections to show", "for users"};
 
 			ListView listView = new ListView(new Rect(0, 1, 20, 10), stringList);
-			parentWindow.Add(listView);
+			return listView;
 		}
 	}
 }
