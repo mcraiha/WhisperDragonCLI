52c7469e52f2e23fa2029a3c60bade6d00010d1f
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-02-10 21:06:20 +0200

    Add EmbedResourceLoader
    For loading resources

---
+++ src/EmbedResourceLoader.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+public static class EmbedResourceLoader
+{
+    public static HashSet<string> ReadAsHashset(string resourceToLoad)
+    {
+        HashSet<string> returnValue = new HashSet<string>();
+
+        Stream namesStream = LoadResourceStream(resourceToLoad, typeof(EmbedResourceLoader).GetTypeInfo().Assembly);
+        using (StreamReader sr = new StreamReader(namesStream)) 
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null) 
+            {
+                if (line.StartsWith("//") || line.Length < 1)
+                {
+                    continue;
+                }
+
+                returnValue.Add(line);
+            }
+        }
+
+        return returnValue;
+    }
+
+    public static List<string> ReadAsList(string resourceToLoad)
+    {
+        List<string> returnValue = new List<string>();
+
+        Stream namesStream = LoadResourceStream(resourceToLoad, typeof(EmbedResourceLoader).GetTypeInfo().Assembly);
+        using (StreamReader sr = new StreamReader(namesStream)) 
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null) 
+            {
+                if (line.StartsWith("//") || line.Length < 1)
+                {
+                    continue;
+                }
+
+                returnValue.Add(line);
+            }
+        }
+
+        return returnValue;
+    }
+
+    private static Stream LoadResourceStream(string resourceName, Assembly assembly)
+    {
+        string properResourceName = GetResourceName(assembly, resourceName);
+        return assembly.GetManifestResourceStream(properResourceName);
+    }
+
+    private static string GetResourceName(Assembly assembly, string resourceName)
+    {
+        return $"{assembly.GetName().Name}.{resourceName.Replace(" ", "_").Replace("\\", ".").Replace("/", ".")}";
+    }
+}
