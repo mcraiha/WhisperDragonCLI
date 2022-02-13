2d024fde07a68c86b7a0e1949622db9f62424af7
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-02-13 12:58:50 +0200

    Add one TODO
    Just to keep it in memory

--- src/LocMan.cs
+++ src/LocMan.cs
@@ -4,6 +4,7 @@ namespace WhisperDragonCLI
     // Localization manager
     public static class LocMan
     {
+        // TODO: Implement the actual one at some point
         public static string Get(string localizationId)
         {
             return localizationId;
