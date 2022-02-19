7655c3b0fdc6777cb1c203964d44906910047105
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-02-19 16:43:40 +0200

    Use better layout for AddOrEditLoginInformationDialog
    Still far from perfect

--- src/AddOrEditLoginInformationDialog.cs
+++ src/AddOrEditLoginInformationDialog.cs
@@ -11,73 +11,96 @@ namespace WhisperDragonCLI
 
 			string okActionText = editMode ? "Edit" : "Add";
 
-			var ok = new Button(1, 14, LocMan.Get(okActionText));
+			var ok = new Button(LocMan.Get(okActionText));
 			ok.Clicked += () => { okAction.Invoke(); };
 
-			var cancel = new Button(1, 14, LocMan.Get("Cancel"));
+			var cancel = new Button(LocMan.Get("Cancel"));
 			cancel.Clicked += () => { cancelAction.Invoke(); };
 
-			var isSecure = new CheckBox(1, 4, "Is Secure", true);
+			var isSecure = new CheckBox("Is Secure", true);
 
-
-			var loginTitleLabel = new Label(1, 1, LocMan.Get("Login title:"));
+			var loginTitleLabel = new Label(LocMan.Get("Login title:"))
+			{
+				X = 0,
+				Y = Pos.Bottom(isSecure) + 1,
+			};
 			var loginTitleTextField = new TextField("") 
 			{
-				X = 1, 
-				Y = 2,
+				X = 0,
+				Y = Pos.Bottom(loginTitleLabel),
 				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			var loginURLLabel = new Label(1, 1, LocMan.Get("Login URL:"));
+			var loginURLLabel = new Label(LocMan.Get("Login URL:"))
+			{
+				X = 0,
+				Y = Pos.Bottom(loginTitleTextField) + 1,
+			};
 			var loginURLTextField = new TextField("") 
 			{
-				X = 1, 
-				Y = 2,
+				X = 0, 
+				Y = Pos.Bottom(loginURLLabel),
 				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			var loginEmailLabel = new Label(1, 1, LocMan.Get("Login email:"));
+			var loginEmailLabel = new Label(LocMan.Get("Login email:"))
+			{
+				X = 0,
+				Y = Pos.Bottom(loginURLTextField) + 1,
+			};
 			var loginEmailTextField = new TextField("") 
 			{
-				X = 1, 
-				Y = 2,
+				X = 0, 
+				Y = Pos.Bottom(loginEmailLabel),
 				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			var loginUsernameLabel = new Label(1, 1, LocMan.Get("Login username:"));
+			var loginUsernameLabel = new Label(LocMan.Get("Login username:"))
+			{
+				X = 0,
+				Y = Pos.Bottom(loginEmailTextField) + 1,
+			};
 			var loginUsernameTextField = new TextField("") 
 			{
-				X = 1, 
-				Y = 2,
+				X = 0, 
+				Y = Pos.Bottom(loginUsernameLabel),
 				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			var loginPasswordLabel = new Label(1, 1, LocMan.Get("Login password:"));
+			var loginPasswordLabel = new Label(LocMan.Get("Login password:"))
+			{
+				X = 0,
+				Y = Pos.Bottom(loginUsernameTextField) + 1,
+			};
 			var loginPasswordTextField = new TextField("") 
 			{
 				Secret = true,
-				X = 1, 
-				Y = 2,
+				X = 0, 
+				Y = Pos.Bottom(loginPasswordLabel),
 				Width = Dim.Fill(),
 				Height = 1
 			};
 
-			var loginNotesLabel = new Label(1, 1, LocMan.Get("Login notes:"));
-			var loginNotesTextField = new TextField("") 
+			var loginNotesLabel = new Label(LocMan.Get("Login notes:"))
 			{
-				X = 1, 
-				Y = 2,
+				X = 0,
+				Y = Pos.Bottom(loginPasswordTextField) + 1,
+			};
+			var loginNotesTextView = new TextView()
+			{
+				X = 0, 
+				Y = Pos.Bottom(loginNotesLabel),
 				Width = Dim.Fill(),
-				Height = 5
+				Height = 3
 			};
 
-			var dialog = new Dialog(LocMan.Get(title), 60, 17, ok, cancel);
+			var dialog = new Dialog(LocMan.Get(title), 0, 0, ok, cancel);
 
-			dialog.Add(isSecure, loginTitleLabel, loginTitleTextField, loginURLLabel, loginURLTextField, loginEmailLabel, loginEmailTextField, loginUsernameLabel, loginUsernameTextField, loginPasswordLabel, loginPasswordTextField, loginNotesLabel, loginNotesTextField);
+			dialog.Add(isSecure, loginTitleLabel, loginTitleTextField, loginURLLabel, loginURLTextField, loginEmailLabel, loginEmailTextField, loginUsernameLabel, loginUsernameTextField, loginPasswordLabel, loginPasswordTextField, loginNotesLabel, loginNotesTextView);
 
 			return dialog;
 		}
